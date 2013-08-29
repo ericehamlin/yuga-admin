0365861e6a324db18c9fd2d81d17e42626eb5d26
Eric Hamlin <ehamlin@grio.com>
2013-08-29 16:25:47 -0700

    remove grunt cdnify because it wasn't adding protocols

--- Gruntfile.js
+++ Gruntfile.js
@@ -346,7 +346,7 @@ module.exports = function (grunt) {
     'autoprefixer',
     'concat',
     'copy:dist',
-    'cdnify',
+    //'cdnify', // not working right now
     'ngmin',
     'cssmin',
     'uglify',
