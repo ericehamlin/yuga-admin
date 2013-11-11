4977be471276ceb871d0ecaefc2d52fc5a568d09
Eric Hamlin <ehamlin@grio.com>
2013-11-11 00:11:34 -0800

    more skeleton

--- test/spec/domain/type.js
+++ test/spec/domain/type.js
@@ -20,5 +20,9 @@ describe('yuga.Type', function () {
     });
 
     describe("clone", function() {
+        it("should make a complete clone of the original type, minus references to other domain objects", function() {
+            var newType = type.clone();
+            //todo test
+        });
     });
 });
